a51a73a89324c85740b2d8ad3d0c3e65d8f752a1
Ubuntu <ubuntu@ip-172-31-48-84.ec2.internal>
2020-07-04 18:46:22 +0000

    push all files

--- zqueuemanagement/app.js
+++ zqueuemanagement/app.js
@@ -4,12 +4,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -19,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/index', indexRouter);
 app.use('/users', usersRouter);
